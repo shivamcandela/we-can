59eb4b270e23b35fbad6cce0b07b1d50cd6d96ba
goyalsaurabh06 <saurabh.goyal@candelatech.com>
2021-10-26 17:48:27 +0530

    Added Enter Button
    Signed-off-by: goyalsaurabh06 <saurabh.goyal@candelatech.com>

--- app/src/main/java/com/candela/wecan/StartupActivity.java
+++ app/src/main/java/com/candela/wecan/StartupActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.candela.wecan.databinding.ActivityStartupBinding;
 
@@ -13,20 +15,29 @@ import com.candela.wecan.databinding.ActivityStartupBinding;
  */
 public class StartupActivity extends AppCompatActivity {
 
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
         getSupportActionBar().hide();
 
+        button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openServerConnection();
+            }
+        });
         /**
          * Switch Activity from here
          */
+    }
+    public void openServerConnection() {
 
         Intent myIntent = new Intent(this, ServerConnection.class);
         startActivity(myIntent);
-
     }
 }
+
 
