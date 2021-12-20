274841da19e1d63de6256232c175d4988a934e39
Ben Greear <greearb@candelatech.com>
2021-12-20 13:57:34 -0800

    scan:  Fix centerfreq1 access for older android.

--- app/src/main/java/com/candela/wecan/ui/home/HomeFragment.java
+++ app/src/main/java/com/candela/wecan/ui/home/HomeFragment.java
@@ -697,10 +697,11 @@ public class HomeFragment extends Fragment {
             String bssid =  sr.BSSID; //Get the BSSID
             String capability = sr.capabilities; //Get Wi-Fi capabilities
             int centerFreq0 = 0;
+            int centerFreq1 = 0;
             if (Build.VERSION.SDK_INT >= 23) {
                centerFreq0 = sr.centerFreq0; //Get centerFreq0
+               centerFreq1 = sr.centerFreq1; //Get centerFreq1
             }
-            int centerFreq1 = sr.centerFreq1; //Get centerFreq1
             int channelWidth = sr.channelWidth; //Get channelWidth
             int level = sr.level; //Get level/rssi
             int frequency = sr.frequency; //Get frequency
